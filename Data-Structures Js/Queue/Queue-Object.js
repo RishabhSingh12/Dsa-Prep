cdd6a638370cb2887a0c55d362540591e7254fb9
Rishabh Kumar <rishabh2807s@gmail.com>
2023-09-23 22:36:02 +0530

    Added necessary methods for the queue-object implementation in js

--- Data-Structures Js/Queue/Queue-Object.js
+++ Data-Structures Js/Queue/Queue-Object.js
@@ -12,4 +12,32 @@ class Queue {
     this.items[this.rear] = value;
     this.rear++;
   }
+
+  //for removing the element from front of the queue
+  dequeue() {
+    const item = this.items[this.front];
+    delete this.items[this.front];
+    this.front++;
+    return item;
+  }
+
+  //checking if the queue is empty
+  isEmpty() {
+    return this.rear - this.front === 0;
+  }
+
+  //checking the top element of a queue
+  peek() {
+    return this.items[this.front];
+  }
+
+  //checking the size of the queue
+  size() {
+    return this.rear - this.front;
+  }
+
+  //printing the queue elements
+  print() {
+    console.table(this.items);
+  }
 }
