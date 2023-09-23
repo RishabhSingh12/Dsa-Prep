7c3fb58dfde42d7326be8d488910d952ed4abb98
Rishabh Kumar <rishabh2807s@gmail.com>
2023-09-23 23:19:59 +0530

    Added enqueue deque and isFull and other funcxtions for the implementation of circular queue in js

--- Data-Structures Js/Cicular-queue.js
+++ Data-Structures Js/Cicular-queue.js
@@ -22,4 +22,57 @@ class CircularQueue {
   }
 
   //adding elememts to the circulatr queue
+  enqueue(item) {
+    if (!this.isFull()) {
+      this.rear = (this.rear + 1) % this.capacity;
+      this.items[this.rear] = item;
+      this.currentLength += 1;
+
+      //if adding the first ever element move the front on position of rear
+      if (this.front === -1) {
+        this.front = this.rear;
+      }
+    } else {
+      console.log("Queue is already full !");
+    }
+  }
+
+  //deleting the elements from the dequeue
+  dequeue() {
+    if (!this.isEmpty()) {
+      const item = this.front;
+      this.items[this.front] = null;
+      this.front = (this.front + 1) % this.capacity;
+      if (this.isEmpty()) {
+        this.front = -1;
+        this.rear = -1;
+      }
+      return item;
+    } else return null;
+  }
+
+  //printing the top of a circular queue
+  peek() {
+    if (!this.isEmpty()) {
+      return this.items[this.front];
+    } else {
+      return null;
+    }
+  }
+
+  print() {
+    if (this.isEmpty()) {
+      console.log("Queue is empty");
+    } else {
+      let i;
+      let str = "";
+      for (let i = this.front; i !== this.rear; i = (i + 1) % this.capacity) {
+        str += this.items[i] + " ";
+      }
+      str = this.items[i];
+      console.log(str);
+    }
+  }
 }
+
+//creating instance
