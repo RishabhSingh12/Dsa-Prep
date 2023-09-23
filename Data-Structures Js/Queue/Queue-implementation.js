ba3e978e43b5fd33f7aa23605f6832492528a5e6
Rishabh Kumar <rishabh2807s@gmail.com>
2023-09-23 22:07:08 +0530

    Added peek , size and print methods for the queueimplementation

--- Data-Structures Js/Queue/Queue-implementation.js
+++ Data-Structures Js/Queue/Queue-implementation.js
@@ -13,4 +13,27 @@ class Queue {
   dequeue() {
     return this.items.shift();
   }
+
+  //to check whether the queue is empty or not
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  //to check for the frontmost element of a queue
+  peek() {
+    if (!this.isEmpty()) {
+      return this.items[0];
+    }
+    return null;
+  }
+
+  //to return the size of the queue
+  size() {
+    return this.items.length;
+  }
+
+  //to return the items in the queue
+  print() {
+    return this.items.toString();
+  }
 }
