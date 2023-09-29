463f02763ca0176c762af7eb627738cac99d06c2
Rishabh Kumar <rishabh2807s@gmail.com>
2023-09-29 22:24:13 +0530

    Added node asnd linkedlist classes for linkedlist implementation in js

---
+++ Data-Structures Js/Linked-list/linked-list.js
@@ -0,0 +1,15 @@
+//node class for linkedlist
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+//linkedlist class
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+}
