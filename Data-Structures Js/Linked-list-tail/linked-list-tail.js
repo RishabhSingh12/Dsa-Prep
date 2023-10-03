53e04afafdc727da6a58302151298bd4547cf9e7
Rishabh Kumar <rishabh2807s@gmail.com>
2023-10-03 22:22:10 +0530

    Added code for the linked list with tail implementation in js

--- Data-Structures Js/Linked-list-tail/linked-list-tail.js
+++ Data-Structures Js/Linked-list-tail/linked-list-tail.js
@@ -1,1 +1,44 @@
-class Node {}
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Linkedlist {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  getSize() {
+    return this.size;
+  }
+
+  print() {
+    if (this.isEmpty()) {
+      console.log("List is enpty!");
+    } else {
+      let prev = this.head;
+      let currValue = "";
+
+      while (prev) {
+        currValue += `${prev.value}`;
+        prev = prev.next;
+      }
+      console.log(currValue);
+    }
+  }
+}
+
+//testing the implementation
+
+const list = new Linkedlist();
+console.log(list.isEmpty());
+console.log(list.getSize());
+list.print();
