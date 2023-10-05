5bcfa935acb4d719b84bb65f5718dbaa8da791bf
Rishabh Kumar <rishabh2807s@gmail.com>
2023-10-05 23:07:41 +0530

    Added removefromfront, removefromend and print methods in linked-list-tail

--- Data-Structures Js/Linked-list-queue/linked-list-tail.js
+++ Data-Structures Js/Linked-list-queue/linked-list-tail.js
@@ -38,12 +38,70 @@ class LinkedList {
   append(val) {
     const node = new Node(val);
     if (this.isEmpty()) {
-      this.head = null;
-      this.tail = null;
+      this.head = node;
+      this.tail = node;
     } else {
       this.tail.next = node;
       this.tail = this.tail.next;
     }
     this.size++;
   }
+
+  //removefromend
+  removeFromEnd() {
+    if (this.isEmpty()) {
+      return null;
+    } else {
+      let prev = this.head;
+      while (prev.next && prev.next !== this.tail) {
+        prev = prev.next;
+      }
+      const val = this.tail.value;
+      prev.next = null;
+      this.tail = prev;
+      this.size--;
+      return val;
+    }
+  }
+
+  //removeFromFront
+  removeFromFront() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    const val = this.head.value;
+    let prev = this.head;
+    this.head = this.head.next;
+    prev.next = null;
+    this.size--;
+    return val;
+  }
+
+  //printing the values
+  print() {
+    if (this.isEmpty()) {
+      console.log("List is empty!");
+    } else {
+      let curr = this.head;
+      let currValue = "";
+      while (curr) {
+        currValue += `${curr.value} `;
+        curr = curr.next;
+      }
+      console.log(currValue);
+    }
+  }
 }
+
+//testing the implementation
+const list = new LinkedList();
+
+list.append(10);
+list.append(20);
+list.append(30);
+
+list.print();
+
+console.log(list.removeFromFront());
+
+list.print();
