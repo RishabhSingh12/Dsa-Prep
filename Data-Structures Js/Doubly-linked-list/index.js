3626f957b15c62c323305bc5c9f392257fce5f92
Rishabh Kumar <rishabh2807s@gmail.com>
2023-10-06 23:34:28 +0530

    Modified logic for append , prepend and added logic for removefromFront method

--- Data-Structures Js/Doubly-linked-list/index.js
+++ Data-Structures Js/Doubly-linked-list/index.js
@@ -22,4 +22,43 @@ class LinkedList {
   getSize() {
     return this.size;
   }
+
+  //adding values to front of a doubly linked list
+  prepend(val) {
+    const node = new Node(val);
+    if (this.isEmpty()) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      node.next = this.head;
+      this.head.prev = node;
+      this.head = node;
+    }
+    this.size++;
+  }
+
+  //adding values to end of a doubly linked list
+  append(val) {
+    const node = new Node(val);
+    if (this.isEmpty()) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      node.prev = this.tail;
+      this.tail = node;
+    }
+    this.size++;
+  }
+
+  //removeFrom front
+  removeFromFront() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    const val = this.head.value;
+    this.head = this.head.next;
+    this.size--;
+    return val;
+  }
 }
