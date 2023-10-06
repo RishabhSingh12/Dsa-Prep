e07b34a432f7a36257060cf6415d2e5852f0eb9e
Rishabh Kumar <rishabh2807s@gmail.com>
2023-10-06 23:22:06 +0530

    Added initial classes for doubly-linked-list implelementation in js

---
+++ Data-Structures Js/Doubly-linked-list/index.js
@@ -0,0 +1,17 @@
+//node class
+class Node {
+  constructor(val) {
+    this.value = val;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+//linkedlist class
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+}
